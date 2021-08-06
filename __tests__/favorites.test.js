c102c27c552020d3fbe3091ab3f1d3e62ffe9893
cmmerritt <christiane.merritt@gmail.com>
2021-08-06 14:44:34 -0700

    add red get all favorites test

--- __tests__/favorites.test.js
+++ __tests__/favorites.test.js
@@ -12,6 +12,14 @@ const delicateDependency = {
   pubDate: '1982-03-28'
 };
 
+const castle = {
+  title: 'We Have Always Lived in the Castle',
+  author: 'Shirley Jackson',
+  imageUrl: 'imageurl',
+  infoUrl: 'infourl',
+  pubDate: '1962-09-21'
+};
+
 describe('favorite routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -28,13 +36,22 @@ describe('favorite routes', () => {
     });
   });
 
-  it('gets a dollhouse by id via GET', async () => {
+  it('gets a favorite by id via GET', async () => {
     const testBook = await Favorite.insert(delicateDependency);
 
     const res = await request(app)
       .get(`/api/v1/favorites/${testBook.id}`);
     
     expect(res.body).toEqual(testBook);
+  });
+
+  it('gets all favorites via GET', async () => {
+    const testBook1 = await Favorite.insert(delicateDependency);
+    const testBook2 = await Favorite.insert(castle);
+
+    const res = await request(app).get('/api/v1/favorites');
+
+    expect(res.body).toEqual([testBook1, testBook2]);
   });
 
 });
